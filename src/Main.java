15e85d0f7acb98c97d91d48da5a3bef201fbfe36
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-14 15:38:53 +0530

    commit a change

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
            //greeting
-           //this is my main method
+           //this is  main method
             System.out.println("hello every one");
             System.out.println("happy learning");
             System.out.println("welcome to git");
