59ba9cfef3a3e61e6715aad17bf70d370b00c42e
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-14 14:20:40 +0530

    add a new feature

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-
-
-
-            //this will print numbers from 1 to 5
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted tex
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
 
