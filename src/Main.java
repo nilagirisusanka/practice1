d3930bd280aa772cc2c4a2a6f97951d3f17ae78f
nilairisusanka <nilagirisusanka@gmail.com>
2024-02-14 17:33:59 +0530

    commit a change im main method

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
+    public static void mgain(String[] args) {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted tex
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
            //greeting
            //this is  main method
             System.out.println("hello every one");
-            System.out.println("happy learning");
-            System.out.println("welcome to git");
+            System.out.println("happy learning!");
+            System.out.println("welcome to git.");
 
     }
 }
